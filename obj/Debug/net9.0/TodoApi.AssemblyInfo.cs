83ebada65b84a39c6437fe9e785f28eebfed6858
Asadell <m.satrioasadell@gmail.com>
2025-12-25 19:33:42 +0700

    chore: migratoin

--- obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43cc89cc7f99cbac51d01e19704f2752a0509e90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e52dd1a7c330db4bdcab50a546ef1fc6aac4b283")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
