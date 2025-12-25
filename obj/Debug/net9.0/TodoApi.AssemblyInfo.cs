e52dd1a7c330db4bdcab50a546ef1fc6aac4b283
Asadell <m.satrioasadell@gmail.com>
2025-12-25 19:17:37 +0700

    chore: model, db context

--- obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5c86bbc79e4ceaeb453ede9f55ea2fdd62c08f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43cc89cc7f99cbac51d01e19704f2752a0509e90")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
